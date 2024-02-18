3ad6f8effcb20ee9442ee4e80ffba87313d7ed26
Yosuke Arimoto <yosuke@arimoto.biz>
2024-02-18 14:03:26 +0900

    bootstrap対応

--- app.py
+++ app.py
@@ -5,26 +5,28 @@ from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = 'EagleIsWashiInJapanese'
-app.permanent_session_lifetime = timedelta(minutes=30)
+app.permanent = True
+app.permanent_session_lifetime = timedelta(days=30)
 
 @app.route('/images/<id>/<file>', methods=['GET'])
 def images(id, file):
     eagle = Eagle(session.get('EagleLibraryPath'))
-    session['EagleLibraryPath'] = eagle.sessiondata()
-    return send_from_directory(path.join(eagle.librarypath(), 'images', id + '.info'), file)
+    folderpath = eagle.folderpath(id)
+    session['EagleLibraryPath'] = eagle.dump()
+    return send_from_directory(folderpath, file)
 
 @app.route('/eagle/<id>', methods=['GET'])
 def eagleimage(id):
     eagle = Eagle(session.get('EagleLibraryPath'))
     img = eagle.getimageinfo(id)
-    session['EagleLibraryPath'] = eagle.sessiondata()
+    session['EagleLibraryPath'] = eagle.dump()
     return render_template('eagleimage.html', img=img)
 
 @app.route('/eagle', methods=['GET'])
 def eagle():
     eagle = Eagle(session.get('EagleLibraryPath'))
     imgs = eagle.loadimages()
-    session['EagleLibraryPath'] = eagle.sessiondata()
+    session['EagleLibraryPath'] = eagle.dump()
     return render_template('images.html', list=imgs)
 
 @app.route('/', methods=['GET'])
--- eagleapi.py
+++ eagleapi.py
@@ -1,5 +1,8 @@
 import requests
-import re
+import json
+import os.path
+
+MAXMETRIC = 256
 
 def checkresponse(result):
     if result.status_code != 200:
@@ -9,15 +12,87 @@ def checkresponse(result):
         raise Exception(json)
     return json
 
-def getimages():
-    imgs = requests.request('get', 'http://localhost:41595/api/item/list?limit=10')
+def _getimages(n = 10):
+    imgs = requests.request('get', f'http://localhost:41595/api/item/list?limit={n}')
     imgs = checkresponse(imgs)
     return imgs['data']
 
-def getlibraries():
+def _getimageinfo(id):
+    imgs = requests.request('get', f'http://localhost:41595/api/item/info?id={id}')
+    imgs = checkresponse(imgs)
+    return imgs['data']
+
+def _getlibraries():
     libs = requests.request('get', 'http://localhost:41595/api/library/history')
     libs = checkresponse(libs)
     return libs['data']
 
+def _refreshthumbnail(id):
+    data = {"id":id}
+    thumbs = requests.request('post', 'http://localhost:41595/api/item/refreshThumbnail', data=json.dumps(data))
+    checkresponse(thumbs)
+
+def _getthumbnail(id):
+    thumbs = requests.request('get', f'http://localhost:41595/api/item/thumbnail?id={id}')
+    thumbs = checkresponse(thumbs)
+    return thumbs['data']
+
+class Eagle:
+    _librarypath = None
+
+    def __init__(self, data = None):
+        self.load(data)
+    
+    def updatelibrarypath(self, id):
+        for p in _getlibraries():
+            if os.path.isdir(os.path.join(p, 'images', id + '.info')):
+                self._librarypath = p
+                return
+    
+    def folderpath(self, id):
+        folderpath = os.path.join(self._librarypath, 'images', id + '.info')
+        if not os.path.isdir(folderpath):
+            self.updatelibrarypath(id)
+            folderpath = os.path.join(self._librarypath, 'images', id + '.info')
+        return folderpath
+        
+    def getimageinfo(self, id):
+        return _getimageinfo(id)
+
+    def sessiondata(self):
+        return self._librarypath
+
+    def loadimages(self, n=100):
+        imgs = _getimages(n)
+        for img in imgs:
+            if img['width'] < img['height']:
+                height = min(img['height'], MAXMETRIC)
+                width = int(img['width'] * height / img['height'])
+            else:
+                width = min(img['width'], MAXMETRIC)
+                height = int(img['height'] * width / img['width'])
+            img['thumbwidth'] = width
+            img['thumbheight'] = height
+            img['folder'] = self.folderpath(img['id'])
+            thumbname = img['name'] + '_thumbnail.' + img['ext']
+            if os.path.isfile(os.path.join(img['folder'], thumbname)):
+                img['thumbname'] = thumbname
+            else:
+                img['thumbname'] = img['name'] + '.' + img['ext']
+        return imgs
+
+    def load(self, data):
+        try:
+            jsdata = json.loads(data) if data != None else {}
+        except:
+            jsdata = {}
+        self._librarypath = jsdata.get('librarypath', '')
+    
+    def dump(self):
+        return json.dumps({
+            "librarypath":self._librarypath,
+            })
+
 if __name__ == "__main__":
-    imgs = getimages()
+    e = Eagle()
+    imgs = e.loadimages()
