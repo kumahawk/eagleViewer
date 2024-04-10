7ab7eb94d4af0d081dc366790ab6eab87fc305a4
Yosuke Arimoto <yosuke@arimoto.biz>
2024-04-10 17:21:59 +0900

    sessionの解放漏れを修正

--- eagleviewer/views.py
+++ eagleviewer/views.py
@@ -10,57 +10,89 @@ app.config["JSON_AS_ASCII"] = False
 
 @app.route('/images/<id>/<file>', methods=['GET'])
 def images(id, file):
-    eagle = Eagle(session.get('EagleLibraryPath'))
-    folderpath = eagle.folderpath(id)
-    session['EagleLibraryPath'] = eagle.dump()
-    return send_from_directory(folderpath, file)
+    try:
+        eagle = Eagle(session.get('EagleLibraryPath'))
+        folderpath = eagle.folderpath(id)
+        session['EagleLibraryPath'] = eagle.dump()
+        return send_from_directory(folderpath, file)
+    finally:
+        eagle = None
 
 @app.route('/eagle', methods=['GET'])
 def eagle_search():
-    eagle = Eagle(session.get('EagleLibraryPath'))
-    imgs = eagle.loadimages(60,folder=request.args.get('folder'), keyword=request.args.get('keyword'), tags=request.args.get('tags'))
-    folders = eagle.loadfolders()
-    foldername = eagle.getfoldername(request.args.get('folder'))
-    session['EagleLibraryPath'] = eagle.dump()
-    return render_template('images.html', list=imgs, folders=folders, foldername=foldername, URLBASE=app.URLBASE)
+    try:
+        eagle = Eagle(session.get('EagleLibraryPath'))
+        imgs = eagle.loadimages(60,folder=request.args.get('folder'), keyword=request.args.get('keyword'), tags=request.args.get('tags'))
+        folders = eagle.loadfolders()
+        foldername = eagle.getfoldername(request.args.get('folder'))
+        session['EagleLibraryPath'] = eagle.dump()
+        return render_template('images.html', list=imgs, folders=folders, foldername=foldername, URLBASE=app.URLBASE)
+    finally:
+        eagle = None
 
 @app.route('/eagle/fetch', methods=['GET'])
 @app.route('/eagle/fetch/<id>', methods=['GET'])
 def eagle_fetch(id = None):
-    eagle = Eagle(session.get('EagleLibraryPath'))
-    imgs = eagle.loadimages(60,skipuntil=id,folder=request.args.get('folder'), keyword=request.args.get('keyword'), tags=request.args.get('tags'))
-    session['EagleLibraryPath'] = eagle.dump()
-    json = {"images":imgs, "finish": (len(imgs) < 60) }
-    return jsonify(json)
+    try:
+        eagle = Eagle(session.get('EagleLibraryPath'))
+        imgs = eagle.loadimages(60,skipuntil=id,folder=request.args.get('folder'), keyword=request.args.get('keyword'), tags=request.args.get('tags'))
+        session['EagleLibraryPath'] = eagle.dump()
+        json = {"images":imgs, "finish": (len(imgs) < 60) }
+        return jsonify(json)
+    finally:
+        eagle = None
 
 @app.route('/eagle/update/<id>', methods=['POST'])
 def eagle_update(id):
-    eagle = Eagle(session.get('EagleLibraryPath'))
-    json = request.get_json()
-    imgs = eagle.update(id, json)
-    session['EagleLibraryPath'] = eagle.dump()
-    return jsonify(imgs)
+    try:
+        eagle = Eagle(session.get('EagleLibraryPath'))
+        json = request.get_json()
+        imgs = eagle.update(id, json)
+        session['EagleLibraryPath'] = eagle.dump()
+        return jsonify(imgs)
+    finally:
+        eagle = None
+
+@app.route('/eagle/delete/<id>', methods=['POST'])
+def eagle_delete(id):
+    try:
+        eagle = Eagle(session.get('EagleLibraryPath'))
+        json = request.get_json()
+        imgs = eagle.delete(id, dict())
+        session['EagleLibraryPath'] = eagle.dump()
+        return jsonify(imgs)
+    finally:
+        eagle = None
 
 @app.route('/eagle/updatedb/start', methods=['POST'])
 def eagle_updatedbstart():
-    eagle = Eagle(session.get('EagleLibraryPath'))
-    response = eagle.updatedb();
-    session['EagleLibraryPath'] = eagle.dump()
-    return jsonify(response)
+    try:
+        eagle = Eagle(session.get('EagleLibraryPath'))
+        response = eagle.updatedb();
+        session['EagleLibraryPath'] = eagle.dump()
+        return jsonify(response)
+    finally:
+        eagle = None
 
 @app.route('/eagle/updatedb/wait', methods=['GET'])
 def eagle_updatedbwait():
-    eagle = Eagle(session.get('EagleLibraryPath'))
-    response = eagle.waitupdatedb();
-    session['EagleLibraryPath'] = eagle.dump()
-    return jsonify(response)
+    try:
+        eagle = Eagle(session.get('EagleLibraryPath'))
+        response = eagle.waitupdatedb();
+        session['EagleLibraryPath'] = eagle.dump()
+        return jsonify(response)
+    finally:
+        eagle = None
 
 @app.route('/eagle/updatedb/abort', methods=['POST'])
 def eagle_updatedbabort():
-    eagle = Eagle(session.get('EagleLibraryPath'))
-    eagle.abortupdatedb();
-    session['EagleLibraryPath'] = eagle.dump()
-    return jsonify({})
+    try:
+        eagle = Eagle(session.get('EagleLibraryPath'))
+        eagle.abortupdatedb();
+        session['EagleLibraryPath'] = eagle.dump()
+        return jsonify({})
+    finally:
+        eagle = None
 
 @app.route('/', methods=['GET'])
 def index():
