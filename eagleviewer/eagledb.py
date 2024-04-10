b5cca3a0716f9489c17ecb5d3caddf225d782ad4
Yosuke Arimoto <yosuke@arimoto.biz>
2024-04-10 23:41:45 +0900

    sqliteの外部キーを有効化

--- eagleviewer/eagledb.py
+++ eagleviewer/eagledb.py
@@ -2,6 +2,7 @@
 import os
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy import create_engine
+from sqlalchemy import event
 
 # SqlAlchemy を使うための共通変数を定義している
 Base = automap_base()
@@ -12,3 +13,9 @@ Images = Base.classes.images
 Folders = Base.classes.folders
 Tags = Base.classes.tags
 Libraries = Base.classes.libraries
+
+@event.listens_for(engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys = ON")
+    cursor.close()
